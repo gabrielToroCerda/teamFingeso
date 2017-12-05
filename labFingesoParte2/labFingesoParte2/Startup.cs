40af059c7145da53bc0b59a12304fec10ee889c9
Gaboe <Gaboe@DESKTOP-578J93A>
2017-12-05 20:20:06 -0300

    no he hecho nada

---
+++ labFingesoParte2/labFingesoParte2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(labFingesoParte2.Startup))]
+namespace labFingesoParte2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
