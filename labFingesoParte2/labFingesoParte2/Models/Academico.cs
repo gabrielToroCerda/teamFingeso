8556e25df79fe5041a51505cc45681bce7c37ab3
Gaboe <Gaboe@DESKTOP-578J93A>
2017-12-06 01:44:51 -0300

    He agregado el modelo, pero falta implementar algunas cosas del diagrama de clases

---
+++ labFingesoParte2/labFingesoParte2/Models/Academico.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labFingesoParte2.Models
+{
+    public class Academico : Usuario
+    {
+    }
+}
